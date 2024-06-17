9aee0c453681770f558a14b02b8faac259567b6a
Yasin Fouladi <mryas3352@gmail.com>
2024-06-17 22:35:14 +0300

    feat: add background image to home page

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:dns_changer/src/util/app_consts.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,7 +12,15 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(AppConsts.backgroundImage),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: const SizedBox.shrink(),
+      ),
     );
   }
 }
