9c86b76bd74f25afb573caf64daae53a8cdeff0a
Yasin Fouladi <mryas3352@gmail.com>
2024-06-18 21:07:34 +0300

    feat: add bordeRadius style to consts

--- lib/src/util/app_consts.dart
+++ lib/src/util/app_consts.dart
@@ -2,4 +2,5 @@ class AppConsts {
   static const String appName = "DNS Changer";
   static const String backgroundImage = "assets/images/background.jpg";
   static const String fontFamily = "Vazirmatn";
+  static const double borderRadius = 12.0;
 }
