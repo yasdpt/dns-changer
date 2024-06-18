5ac85a247d56da9fe1c2d7ae63ffe51afefd3b9d
Yasin Fouladi <mryas3352@gmail.com>
2024-06-18 21:07:19 +0300

    feat: add custom dropdown button

---
+++ lib/src/widgets/custom_dropdown_button.dart
@@ -0,0 +1,59 @@
+import 'package:dns_changer/src/util/app_consts.dart';
+import 'package:flutter/material.dart';
+
+// Custom UI for [DropdownButton] by wrapping it with [Container]
+class CustomDropdownButton extends StatelessWidget {
+  const CustomDropdownButton({
+    super.key,
+    required this.value,
+    required this.items,
+    required this.onChanged,
+  });
+  final String value;
+  final List<String> items;
+  final Function(String? value) onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(AppConsts.borderRadius),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<String>(
+          value: value,
+          icon: const Icon(Icons.arrow_drop_down_rounded),
+          elevation: 0,
+          style: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.normal,
+            fontFamily: AppConsts.fontFamily,
+            color: Colors.black,
+          ),
+          borderRadius: BorderRadius.circular(
+            AppConsts.borderRadius,
+          ),
+          dropdownColor: Theme.of(context).colorScheme.background,
+          onChanged: onChanged,
+          items: items.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(
+                value,
+                style: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.normal,
+                  fontFamily: AppConsts.fontFamily,
+                  color: Colors.black,
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
