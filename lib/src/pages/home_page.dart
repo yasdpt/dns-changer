2ec077f1481fb6296a20042a4a0a4bb358751c01
Yasin Fouladi <mryas3352@gmail.com>
2024-06-18 21:56:54 +0300

    feat: add network adapters dropdown to home page

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,9 @@
+import 'package:dns_changer/src/util/dns_util.dart';
+import 'package:dns_changer/src/widgets/custom_dropdown_button.dart';
+import 'package:flutter/material.dart';
+
 import 'package:dns_changer/src/util/app_consts.dart';
 import 'package:dns_changer/src/widgets/app_header_widget.dart';
-import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +13,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String> _adapters = [""];
+
+  String _selectedAdapter = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration.zero, () {
+      _populateData();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,11 +42,35 @@ class _HomePageState extends State<HomePage> {
           children: [
             const AppHeaderWidget(),
             Expanded(
-              child: Container(),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomDropdownButton(
+                    value: _selectedAdapter,
+                    items: _adapters,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedAdapter = value ?? "";
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 18.0),
+                ],
+              ),
             )
           ],
         ),
       ),
     );
+  }
+
+  void _populateData() async {
+    final adapters = await DNSUtil.getInterfaceNames();
+
+    setState(() {
+      _adapters.clear();
+      _adapters.addAll(adapters);
+      _selectedAdapter = _adapters.first;
+    });
   }
 }
