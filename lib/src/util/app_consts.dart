b35b54ae5249730ba5d7b3b777c1a22af9821d6e
Yasin Fouladi <mryas3352@gmail.com>
2024-06-18 11:14:54 +0300

    feat: add appName and fontFamily consts

--- lib/src/util/app_consts.dart
+++ lib/src/util/app_consts.dart
@@ -1,3 +1,5 @@
 class AppConsts {
+  static const String appName = "DNS Changer";
   static const String backgroundImage = "assets/images/background.jpg";
+  static const String fontFamily = "Vazirmatn";
 }
