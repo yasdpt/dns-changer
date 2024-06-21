ef2ffd704fcd9a9b4114bcf802b95ce6ceb0020c
Yasin Fouladi <mryas3352@gmail.com>
2024-06-21 14:23:49 +0300

    feat: add app icons

--- lib/src/util/app_consts.dart
+++ lib/src/util/app_consts.dart
@@ -1,6 +1,15 @@
 class AppConsts {
   static const String appName = "DNS Changer";
-  static const String backgroundImage = "assets/images/background.jpg";
+  static const String appVersion = "v0.1";
   static const String fontFamily = "Vazirmatn";
   static const double borderRadius = 12.0;
+
+  // Assets
+  static const String icons = "assets/icons";
+  static const String copyIcon = "$icons/copy.png";
+  static const String dnsIcon = "$icons/dns.png";
+  static const String githubIcon = "$icons/github.png";
+  static const String infoIcon = "$icons/info.png";
+  static const String settingsIcon = "$icons/settings.png";
+  static const String updateIcon = "$icons/update.png";
 }
