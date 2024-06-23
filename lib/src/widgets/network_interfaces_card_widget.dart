55d3e935ccc3aa88e7974036405bd4493cd33bbf
Yasin Fouladi <mryas3352@gmail.com>
2024-06-23 23:04:32 +0300

    feat: add linux UI to network interfaces card

--- lib/src/widgets/network_interfaces_card_widget.dart
+++ lib/src/widgets/network_interfaces_card_widget.dart
@@ -1,23 +1,43 @@
+import 'dart:io';
+
 import 'package:dns_changer/src/models/network_interface_model.dart';
 import 'package:dns_changer/src/util/app_consts.dart';
 import 'package:dns_changer/src/styles/app_sizes.dart';
 import 'package:dns_changer/src/util/dns_util.dart';
 import 'package:dns_changer/src/styles/text_styles.dart';
+import 'package:dns_changer/src/util/provider.dart';
 import 'package:dns_changer/src/widgets/custom_dropdown_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class NetworkInterfacesCardWidget extends StatefulWidget {
+class NetworkInterfacesCardWidget extends ConsumerWidget {
   const NetworkInterfacesCardWidget({super.key});
 
   @override
-  State<NetworkInterfacesCardWidget> createState() =>
-      _NetworkInterfacesCardWidgetState();
+  Widget build(BuildContext context, WidgetRef ref) {
+    DNSUtil dnsUtil = ref.read(myDNSUtilProvider);
+
+    return Container(
+      width: 322.0,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.secondary,
+        borderRadius: BorderRadius.circular(AppConsts.borderRadius),
+      ),
+      padding: const EdgeInsets.all(12.0),
+      child: Platform.isWindows ? _CardWindows(dnsUtil) : _CardLinux(dnsUtil),
+    );
+  }
 }
 
-class _NetworkInterfacesCardWidgetState
-    extends State<NetworkInterfacesCardWidget> {
-  final List<NetworkInterfaceModel> _interfaces = [NetworkInterfaceModel()];
+class _CardWindows extends StatefulWidget {
+  const _CardWindows(this.dnsUtil);
+  final DNSUtil dnsUtil;
+  @override
+  State<_CardWindows> createState() => _CardWindowsState();
+}
 
+class _CardWindowsState extends State<_CardWindows> {
+  final List<NetworkInterfaceModel> _interfaces = [NetworkInterfaceModel()];
   NetworkInterfaceModel _selectedInterface = NetworkInterfaceModel();
 
   @override
@@ -31,36 +51,30 @@ class _NetworkInterfacesCardWidgetState
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.secondary,
-        borderRadius: BorderRadius.circular(AppConsts.borderRadius),
-      ),
-      padding: const EdgeInsets.all(12.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsetsDirectional.only(start: 8.0),
-            child: Text(
-              "Select network interface",
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsetsDirectional.only(start: 8.0),
+          child: Text(
+            "Select network interface",
+            style: Theme.of(context).textTheme.bodyLarge,
           ),
-          gapH8,
-          CustomDropdownButton(
-            value: _selectedInterface.name,
-            items: _interfaces.map((element) => element.name).toList(),
-            onChanged: (value) {
-              setState(() {
-                _selectedInterface = _interfaces.firstWhere(
-                  (inteface) => inteface.name == value,
-                );
-              });
-            },
-          ),
-          gapH16,
-          Padding(
+        ),
+        gapH8,
+        CustomDropdownButton(
+          value: _selectedInterface.name,
+          items: _interfaces.map((element) => element.name).toList(),
+          onChanged: (value) {
+            setState(() {
+              _selectedInterface = _interfaces.firstWhere(
+                (inteface) => inteface.name == value,
+              );
+            });
+          },
+        ),
+        gapH16,
+        Padding(
             padding: const EdgeInsetsDirectional.only(start: 8.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -125,20 +139,88 @@ class _NetworkInterfacesCardWidgetState
                 ),
                 gapH8,
               ],
-            ),
-          ),
-        ],
-      ),
+            )),
+      ],
     );
   }
 
   void _populateData() async {
-    final interfaces = await DNSUtil.getInterfaceNames();
+    final interfaces = await widget.dnsUtil.getNetworkInterfacesList();
 
     setState(() {
       _interfaces.clear();
       _interfaces.addAll(interfaces);
       _selectedInterface = _interfaces.first;
+    });
+  }
+}
+
+class _CardLinux extends StatefulWidget {
+  const _CardLinux(this.dnsUtil);
+
+  final DNSUtil dnsUtil;
+
+  @override
+  State<_CardLinux> createState() => _CardLinuxState();
+}
+
+class _CardLinuxState extends State<_CardLinux> {
+  String _interfaceInfo = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration.zero, () {
+      _populateData();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsetsDirectional.only(start: 8.0),
+          child: Text(
+            "Network info",
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsetsDirectional.only(start: 8.0),
+          child: Column(
+            children: [
+              gapH12,
+              Row(
+                children: [
+                  Text(
+                    "DNS Servers:",
+                    style: Theme.of(context).textTheme.bodyMedium?.medium,
+                  ),
+                  gapW8,
+                  Text(
+                    _interfaceInfo.isEmpty
+                        ? "Configured though DHCP"
+                        : _interfaceInfo,
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ],
+              ),
+              gapH8,
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _populateData() async {
+    final currentDNSServers = await widget.dnsUtil.getCurrentDNSServers();
+
+    setState(() {
+      _interfaceInfo = currentDNSServers.join(" ").trim();
     });
   }
 }
