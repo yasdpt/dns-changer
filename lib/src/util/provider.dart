dfc2ba8b1c6a90b28eb7c5dcba0590193602f7f1
Yasin Fouladi <mryas3352@gmail.com>
2024-06-23 23:05:48 +0300

    fix: update dns util provider to avoid conflicts

--- lib/src/util/provider.dart
+++ lib/src/util/provider.dart
@@ -8,7 +8,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'provider.g.dart';
 
 @riverpod
-DNSUtil dnsUtil(DnsUtilRef _) {
+DNSUtil myDNSUtil(MyDNSUtilRef _) {
   if (Platform.isLinux) return DNSUtilLinuxImpl();
 
   return DNSUtilWindowsImpl();
--- lib/src/util/provider.g.dart
+++ lib/src/util/provider.g.dart
@@ -6,19 +6,19 @@ part of 'provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$dnsUtilHash() => r'8b68b19ca7faa903866af35ff8dadaf20a7299e4';
+String _$myDNSUtilHash() => r'049888f5c51fe54f24bbd193d763f2d8d09f28e1';
 
-/// See also [dnsUtil].
-@ProviderFor(dnsUtil)
-final dnsUtilProvider = AutoDisposeProvider<DNSUtil>.internal(
-  dnsUtil,
-  name: r'dnsUtilProvider',
+/// See also [myDNSUtil].
+@ProviderFor(myDNSUtil)
+final myDNSUtilProvider = AutoDisposeProvider<DNSUtil>.internal(
+  myDNSUtil,
+  name: r'myDNSUtilProvider',
   debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$dnsUtilHash,
+      const bool.fromEnvironment('dart.vm.product') ? null : _$myDNSUtilHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef DnsUtilRef = AutoDisposeProviderRef<DNSUtil>;
+typedef MyDNSUtilRef = AutoDisposeProviderRef<DNSUtil>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
