d38e8667109202b062bcd76f5a27e1816ce08e16
Yasin Fouladi <mryas3352@gmail.com>
2024-06-23 15:54:43 +0300

    feat: add network interface model

---
+++ lib/src/models/network_interface_model.dart
@@ -0,0 +1,15 @@
+class NetworkInterfaceModel {
+  final String name;
+  final String adminState;
+  final String state;
+  final String type;
+  final String dnsServers;
+
+  NetworkInterfaceModel({
+    this.name = "",
+    this.adminState = "",
+    this.state = "",
+    this.type = "",
+    this.dnsServers = "",
+  });
+}
