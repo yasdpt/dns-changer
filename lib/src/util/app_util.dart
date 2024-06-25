bacd9aac96aa23865a4453e8d9cf9c18b6f51387
Yasin Fouladi <mryas3352@gmail.com>
2024-06-25 21:43:12 +0300

    feat: add latency indicator widget

--- lib/src/util/app_util.dart
+++ lib/src/util/app_util.dart
@@ -1,5 +1,15 @@
+import 'package:dns_changer/src/styles/app_colors.dart';
+import 'package:flutter/material.dart';
+
 class AppUtil {
   static final _whitespaceRE = RegExp(r"\s+");
   static String cleanupWhitespace(String input) =>
       input.replaceAll(_whitespaceRE, " ");
+
+  static Color getLatencyColor(num latency) {
+    if (latency <= 70) return AppColors.green;
+    if (latency <= 200) return AppColors.orange;
+
+    return AppColors.red;
+  }
 }
---
+++ lib/src/widgets/latency_indicator_widget.dart
@@ -0,0 +1,39 @@
+import 'package:dns_changer/src/util/app_util.dart';
+import 'package:flutter/material.dart';
+
+class LatencyIndicatorWidget extends StatelessWidget {
+  const LatencyIndicatorWidget({super.key, required this.latency});
+
+  final num latency;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 4.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            width: 2,
+            height: 6,
+            color: AppUtil.getLatencyColor(latency),
+          ),
+          if (latency <= 200)
+            Container(
+              width: 2,
+              height: 8,
+              color: AppUtil.getLatencyColor(latency),
+              margin: const EdgeInsetsDirectional.only(start: 2),
+            ),
+          if (latency <= 70)
+            Container(
+              width: 2,
+              height: 10,
+              margin: const EdgeInsetsDirectional.only(start: 2),
+              color: AppUtil.getLatencyColor(latency),
+            )
+        ],
+      ),
+    );
+  }
+}
