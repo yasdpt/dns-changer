880f31689edd33946bd7a678b2ad81ab4189bb53
Yasin Fouladi <mryas3352@gmail.com>
2024-06-26 19:53:29 +0300

    feat: add functionality for side bar widget

--- lib/src/widgets/side_bar_widget.dart
+++ lib/src/widgets/side_bar_widget.dart
@@ -1,13 +1,18 @@
+import 'package:dns_changer/src/controllers/page_controller.dart';
 import 'package:dns_changer/src/util/app_consts.dart';
 import 'package:dns_changer/src/styles/app_sizes.dart';
 import 'package:dns_changer/src/widgets/icon_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class SideBarWidget extends StatelessWidget {
+class SideBarWidget extends ConsumerWidget {
   const SideBarWidget({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentPage = ref.watch(pageControllerProvider);
+
     return Row(
       children: [
         gapW8,
@@ -19,20 +24,23 @@ class SideBarWidget extends StatelessWidget {
                   gapH8,
                   IconWidget(
                     icon: AppConsts.dnsIcon,
-                    isSelected: true,
-                    onTap: () {},
+                    isSelected: currentPage == 0,
+                    onTap: () =>
+                        ref.read(pageControllerProvider.notifier).setPage(0),
                   ),
                   gapH8,
                   IconWidget(
                     icon: AppConsts.settingsIcon,
-                    isSelected: false,
-                    onTap: () {},
+                    isSelected: currentPage == 1,
+                    onTap: () =>
+                        ref.read(pageControllerProvider.notifier).setPage(1),
                   ),
                   gapH8,
                   IconWidget(
                     icon: AppConsts.infoIcon,
-                    isSelected: false,
-                    onTap: () {},
+                    isSelected: currentPage == 2,
+                    onTap: () =>
+                        ref.read(pageControllerProvider.notifier).setPage(2),
                   ),
                 ],
               ),
@@ -40,7 +48,7 @@ class SideBarWidget extends StatelessWidget {
             IconWidget(
               icon: AppConsts.githubIcon,
               color: Theme.of(context).textTheme.bodySmall?.color,
-              onTap: () {},
+              onTap: () => _launchGithubUrl(),
             ),
             gapH8,
           ],
@@ -55,5 +63,11 @@ class SideBarWidget extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  Future<void> _launchGithubUrl() async {
+    if (!await launchUrl(Uri.parse(AppConsts.appGithubUrl))) {
+      throw Exception('Could not launch github url');
+    }
   }
 }
