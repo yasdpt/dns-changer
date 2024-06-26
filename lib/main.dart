7f7f20149806c1a532ed52ffd9138b733d3bfd40
Yasin Fouladi <mryas3352@gmail.com>
2024-06-26 19:55:51 +0300

    feat: config startup settings and theme in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,38 @@
+import 'dart:io';
+
+import 'package:dns_changer/src/controllers/theme_controller.dart';
 import 'package:dns_changer/src/pages/main_page.dart';
 import 'package:dns_changer/src/styles/app_themes.dart';
+import 'package:dns_changer/src/util/app_consts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:launch_at_startup/launch_at_startup.dart';
 import 'package:window_manager/window_manager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  _setupStartup();
   await _setupWindow();
 
   runApp(const ProviderScope(child: MainApp()));
+}
+
+void _setupStartup() {
+  launchAtStartup.setup(
+    appName: AppConsts.appName,
+    appPath: Platform.resolvedExecutable,
+    packageName: 'dev.yasdpt.dnschanger',
+  );
 }
 
 Future<void> _setupWindow() async {
   await windowManager.ensureInitialized();
 
   WindowOptions windowOptions = const WindowOptions(
-    size: Size(412, 670),
-    minimumSize: Size(412, 670),
-    maximumSize: Size(412, 670),
+    size: Size(400, 670),
+    minimumSize: Size(400, 670),
+    maximumSize: Size(400, 670),
     center: true,
     backgroundColor: Colors.transparent,
     titleBarStyle: TitleBarStyle.hidden,
@@ -31,14 +45,16 @@ Future<void> _setupWindow() async {
   });
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(themeControllerProvider);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
+      themeMode: theme,
       theme: AppThemes.lightTheme,
       darkTheme: AppThemes.darkTheme,
       home: const MainPage(),
