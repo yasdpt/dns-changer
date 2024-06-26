d6b52c0138cf227046dc0f61d80bb0a95f98baca
Yasin Fouladi <mryas3352@gmail.com>
2024-06-26 19:56:26 +0300

    feat: add minimize to tray management methods

--- lib/src/pages/main_page.dart
+++ lib/src/pages/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:dns_changer/src/controllers/page_controller.dart';
+import 'package:dns_changer/src/controllers/tray_controller.dart';
 import 'package:dns_changer/src/pages/about_page.dart';
 import 'package:dns_changer/src/pages/dns_page.dart';
 import 'package:dns_changer/src/pages/settings_page.dart';
@@ -8,13 +9,43 @@ import 'package:dns_changer/src/widgets/app_header_widget.dart';
 import 'package:dns_changer/src/widgets/side_bar_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tray_manager/tray_manager.dart';
 
-class MainPage extends ConsumerWidget {
+class MainPage extends ConsumerStatefulWidget {
   const MainPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends ConsumerState<MainPage> with TrayListener {
+  @override
+  void initState() {
+    trayManager.addListener(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    trayManager.removeListener(this);
+    super.dispose();
+  }
+
+  @override
+  void onTrayIconMouseDown() {
+    trayManager.popUpContextMenu();
+  }
+
+  @override
+  void onTrayIconRightMouseDown() {
+    trayManager.popUpContextMenu();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final page = ref.watch(pageControllerProvider);
+    // Watch for state taking effect on startup
+    ref.watch(trayControllerProvider);
 
     return Scaffold(
       backgroundColor: Colors.transparent,
