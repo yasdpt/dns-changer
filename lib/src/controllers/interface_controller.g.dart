c12e0182b69b7d08abb82385bd1627fecf723d1c
Yasin Fouladi <mryas3352@gmail.com>
2024-06-27 16:12:25 +0300

    feat: add dns provider and interface controllers

---
+++ lib/src/controllers/interface_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'interface_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$interfaceControllerHash() =>
+    r'a2d4eab82d374937483a4d071ddada2e54802e0f';
+
+/// See also [InterfaceController].
+@ProviderFor(InterfaceController)
+final interfaceControllerProvider = AutoDisposeNotifierProvider<
+    InterfaceController, NetworkInterfaceModel>.internal(
+  InterfaceController.new,
+  name: r'interfaceControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$interfaceControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$InterfaceController = AutoDisposeNotifier<NetworkInterfaceModel>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
