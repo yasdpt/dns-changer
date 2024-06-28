865830638826d1188fdb88cad67aee0abceb1f31
Yasin Fouladi <mryas3352@gmail.com>
2024-06-28 10:58:11 +0300

    feat: implement about page

--- lib/src/pages/about_page.dart
+++ lib/src/pages/about_page.dart
@@ -1,3 +1,7 @@
+import 'package:dns_changer/src/styles/app_sizes.dart';
+import 'package:dns_changer/src/styles/text_styles.dart';
+import 'package:dns_changer/src/util/app_consts.dart';
+import 'package:dns_changer/src/widgets/icon_widget.dart';
 import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
@@ -5,6 +9,32 @@ class AboutPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        gapH16,
+        Image.asset(
+          AppConsts.logoPNG,
+          width: 60,
+          height: 60,
+          fit: BoxFit.cover,
+        ),
+        gapH16,
+        Text(
+          "Open-Source multiplatform DNS Changer",
+          style: Theme.of(context).textTheme.bodyMedium?.bold,
+        ),
+        gapH8,
+        Text(
+          "Version ${AppConsts.appVersion}",
+          style: Theme.of(context).textTheme.bodySmall,
+        ),
+        gapH8,
+        IconWidget(
+          icon: AppConsts.githubIcon,
+          color: Theme.of(context).textTheme.bodySmall?.color,
+          onTap: () {},
+        ),
+      ],
+    );
   }
 }
